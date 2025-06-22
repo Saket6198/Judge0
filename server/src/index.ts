077b3677aade2c3942ac78d77e9f2e065ca4b9a8
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-06-22 20:47:02 +0530

    added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,37 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import submitRouter from './routes/submit';
-import main from './config/db';
-import cookieParser from 'cookie-parser';
-import problemRouter from './routes/problem';
-import {authRouter} from './routes/user_authentication'
-import {connectRedis} from './config/redis'
-
+import express from "express";
+import dotenv from "dotenv";
+import submitRouter from "./routes/submit";
+import main from "./config/db";
+import cookieParser from "cookie-parser";
+import problemRouter from "./routes/problem";
+import { authRouter } from "./routes/user_authentication";
+import { connectRedis } from "./config/redis";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use('/user', authRouter);
-app.use('/problem', problemRouter);
-app.use('/submission', submitRouter);
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use("/user", authRouter);
+app.use("/problem", problemRouter);
+app.use("/submission", submitRouter);
 
 main()
-    .then(async () => {
-        await connectRedis();
-        console.log('Connected to MongoDB');
-        app.listen(process.env.PORT_NUMBER, () => {
-        console.log(`Server is running on port ${process.env.PORT_NUMBER}`);
-        })
-    })
-    .catch((err) => {
-        console.log("error connecting to the database:", err);
-    })
-
+  .then(async () => {
+    await connectRedis();
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT_NUMBER, () => {
+      console.log(`Server is running on port ${process.env.PORT_NUMBER}`);
+    });
+  })
+  .catch((err) => {
+    console.log("error connecting to the database:", err);
+  });
