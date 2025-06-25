71878703d2f91b98fd1828c98da9421f8be6ea45
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-06-25 22:29:18 +0530

    updated problem model, tag enum to array of string

--- server/src/models/problems_model.ts
+++ server/src/models/problems_model.ts
@@ -2,86 +2,112 @@ import { time, timeStamp } from "console";
 import mongoose, { Schema } from "mongoose";
 import { ref } from "process";
 
-const problemSchema = new mongoose.Schema({
+const problemSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     tags: {
-        type: [String],
-        enum: ['array', 'math', 'string', 'linked-list', 'tree', 'graph', 'dynamic-programming', 'greedy', 'backtracking', 'sorting', 'searching'],
-        required: true,
+      type: [String],
+      enum: [
+        "array",
+        "string",
+        "tree",
+        "graph",
+        "dynamic programming",
+        "greedy",
+        "backtracking",
+        "stack",
+        "queue",
+        "heap",
+        "linked list",
+        "math",
+        "bit manipulation",
+        "recursion",
+        "hash table",
+        "sliding window",
+        "two pointers",
+        "binary search",
+        "sorting",
+        "divide and conquer"
+      ],
+      required: true,
     },
     difficulty: {
-        type: String,
-        enum: ['easy', 'medium', 'hard'],
-        required: true,
+      type: String,
+      enum: ["easy", "medium", "hard"],
+      required: true,
     },
     visibleTestCases: [
-        {
-            input: {
-                type: String,
-                required: true,
-            },
-            output: {
-                type: String,
-                required: true,
-            },
-            explanation: {
-                type: String,
-                required: true,
-            }
-        }
+      {
+        input: {
+          type: String,
+          required: true,
+        },
+        output: {
+          type: String,
+          required: true,
+        },
+        explanation: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     HiddenTestCases: [
-        {
-            input: {
-                type: String,
-                required: true,
-            },
-            output: {
-                type: String,
-                required: true,
-            }
-        }
+      {
+        input: {
+          type: String,
+          required: true,
+        },
+        output: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     startCode: [
-        {
-            language: {
-                type: String,
-                enum: ['c', 'c++', 'java', 'python', 'javascript'],
-                required: true,
-            },
-            initialCode: {
-                type: String,
-                required: true,
-            }
-        }
+      {
+        language: {
+          type: String,
+          enum: ["c", "c++", "java", "python", "javascript"],
+          required: true,
+        },
+        initialCode: {
+          type: String,
+          required: true,
+        },
+      },
     ],
-    referenceSolutions: [
-        {
-            language: {
-                type: String,
-            },
-            solution: { 
-                type: String,
-                required: true,
-            }
-        }
+    referenceSolution: [
+      {
+        language: {
+          type: String,
+          enum: ["c", "c++", "java", "python", "javascript"],
+          required: true,
+        },
+        solution: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     problemCreator: {
-        type: Schema.Types.ObjectId,
-        ref: 'user',
-        required: true,
-    }
-}, {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  {
     timestamps: true,
-});
+  }
+);
 
-const problemModel = mongoose.model('problem', problemSchema);
+const problemModel = mongoose.model("problem", problemSchema);
 
 export default problemModel;
