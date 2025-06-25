8476e2962f344ca70b7e94f8e79e05589ccfd13d
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-06-25 22:30:46 +0530

    add App component with routing and authentication handling

---
+++ client/judge0/src/App.tsx
@@ -0,0 +1,63 @@
+import { Routes, Route, Navigate } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { Login } from "./pages/Login";
+import { SignUp } from "./pages/SignUp";
+import { checkAuth } from "./authSlice";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import canvaSvg from "./assets/canva.svg";
+import { Profile } from "./pages/Profile";
+import { Admin } from "./pages/Admin";
+import { ProblemPage } from "./pages/ProblemPage";
+
+function App() {
+  const dispatch = useAppDispatch();
+  const { authenticated, loading, user } = useAppSelector((state) => state.auth); // properly typed with useAppSelector
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, [dispatch]);
+  if (loading) {
+    return (
+      <div className="min-h-screen items-center justify-center flex flex-col gap-3">
+        <img
+          src={canvaSvg}
+          alt="judge0-loading-icon"
+          width={60}
+          height={60}
+          className="animate-bounce"
+        />
+      </div>
+    );
+  }
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={authenticated ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={authenticated ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authenticated ? <Navigate to="/" /> : <SignUp />}
+        />
+        <Route
+          path="/profile"
+          element={authenticated ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/admin"
+          element={authenticated && user.role === "admin" ? <Admin/> : <HomePage />}
+        />
+        <Route path="/problem/:problemById" element={<ProblemPage />} />
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
