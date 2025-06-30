c5601c93152a5d108c1618bc31549d36a1b6ac81
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-07-01 01:04:41 +0530

    prod changes

--- client/judge0/src/utils/axiosClient.ts
+++ client/judge0/src/utils/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: import.meta.env.BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true, // cookies are sent with requests
   headers: {
     "Content-Type": "application/json",
